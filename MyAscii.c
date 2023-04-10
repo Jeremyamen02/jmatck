885f41edb0f4b150ff09e34dbe060a973d4ae363
Jeremyamen02 <Jeremyamen02@gmail.com>
2023-04-10 12:05:57 -0500

    Added MyAscii and MyAscii.c

---
+++ MyAscii.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    printf("ASCII value of %c = %d", c, c);
+
+    return 0;
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"ascii.c" 10L, 174C                                                                                1,1           All
+
